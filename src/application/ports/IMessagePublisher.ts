1b2dd28a2d31e5a23ce73124feecd5ea574a7a89
trungh97 <hoangquoctrungit@gmail.com>
2025-08-12 21:40:04 +0700

    Task #8: Message publisher initialization

---
+++ src/application/ports/IMessagePublisher.ts
@@ -0,0 +1,15 @@
+import { Conversation, Message } from "@domain/entities";
+
+export interface IMessagePublisher {
+  /**
+   * Publishes a new message to a conversation. This method is used to publish
+   * a message to all connected clients that are subscribed to the conversation.
+   * @param message The message to publish.
+   * @param conversation The conversation to publish the message to.
+   * @returns A Promise that resolves when the message has been published.
+   */
+  publishNewMessage(
+    message: Message,
+    conversation: Conversation
+  ): Promise<void>;
+}
